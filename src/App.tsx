df2b6577be038aeb9f38303251e51551ec65836d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-06-09 15:53:29 -0300

    feat: Routes implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
+import React from 'react';
+import RoutesApp from './RoutesApp';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  )
+export default function App(): React.FunctionComponentElement<{}> {
+  return <RoutesApp />;
 }
